7fd4a1aa1ab4d22c8c6edfe28e7728b9f3a57a77
kaloubeck <kamalammouri1994@gmail.com>
2021-07-03 10:57:53 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let menu_btn = document.querySelector(".btn_bar");
+let cancel_btn = document.querySelector(".cancel_btn");
+let menu_bar = document.querySelector(".menu");
+
+menu_btn.onclick = function() {
+    menu_btn.style.opacity = "0";
+    menu_btn.style.pointerEvents = "none";
+    menu_bar.style.left = "0";
+
+}
+
+
+cancel_btn.onclick = function() {
+    menu_btn.style.opacity = "1";
+    menu_btn.style.pointerEvents = "auto";
+    menu_bar.style.left = "-100%";
+}
