453ed898140aee9798c6af9420fb7e71c527bf18
kaloubeck <kamalammouri1994@gmail.com>
2021-07-04 12:47:36 +0100

    slider

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
-let menu_btn = document.querySelector(".btn_bar");
+let bar_btn = document.querySelector(".btn_bar");
 let cancel_btn = document.querySelector(".cancel_btn");
-let menu_bar = document.querySelector(".navbar_lg");
+let bar_bar = document.querySelector(".navbar_lg");
+let bar_btn2 = document.querySelector(".btn_bar_2");
 
-menu_btn.onclick = function() {
-    menu_btn.style.opacity = "0";
-    menu_btn.style.pointerEvents = "none";
-    menu_bar.style.left = "0";
+
+bar_btn2.onclick = function() {
+    bar_btn2.style.opacity = "0";
+    bar_btn2.style.pointerEvents = "none";
+    bar_bar.style.left = "0";
+
+}
+
+bar_btn.onclick = function() {
+    bar_btn.style.opacity = "0";
+    bar_btn.style.pointerEvents = "none";
+    bar_bar.style.left = "0";
+
+}
+cancel_btn.onclick = function() {
+
+    bar_bar.style.left = "-100%";
+
+    bar_btn.style.opacity = "1";
+    bar_btn.style.pointerEvents = "auto";
+
+    bar_btn2.style.opacity = "1";
+    bar_btn2.style.pointerEvents = "auto";
 
 }
 
 
-cancel_btn.onclick = function() {
-    menu_btn.style.opacity = "1";
-    menu_btn.style.pointerEvents = "auto";
-    menu_bar.style.left = "-100%";
+
+
+let btn_bar2 = document.querySelector(".btn_bar_2");
+window.onscroll = function() {
+    if (document.scrollingElement.scrollTop > 35) {
+        btn_bar2.classList.add('btn_bar_2_active');
+    } else {
+        btn_bar2.classList.remove('btn_bar_2_active');
+    }
 }
